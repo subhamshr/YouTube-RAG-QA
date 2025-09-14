3a18c5a8db5b8e55611348ea10946ca8005e3690
Subham Shrestha <subham.shrestha13@gmail.com>
2025-09-14 18:31:15 +0545

    Fetch Transcript and upsert into Pinecone

---
+++ upsert_embeddings.py
@@ -0,0 +1,33 @@
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound, VideoUnavailable
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from pinecone_client import index
+from .embedding import embeddings_model
+
+video_id = "YSMWN8VpY6A"
+ytt_api = YouTubeTranscriptApi()
+
+#1.Fetch transcript
+try:
+    transcript_list = ytt_api.fetch(video_id, languages=["en"])
+    transcript = " ".join(snippet.text for snippet in transcript_list.snippets)
+except TranscriptsDisabled:
+    raise Exception("Captions are disabled for this video.")
+except NoTranscriptFound:
+    raise Exception("No transcript found for this video.")
+except VideoUnavailable:
+    raise Exception("The video is unavailable.")
+
+#2.Split transcript into chunks
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = splitter.create_documents([transcript])
+
+#3.Generate embeddings
+embeddings_list = [embeddings_model.embed_query(chunk.page_content) for chunk in chunks]
+
+#4.Upsert into Pinecone
+vectors_to_upsert = [
+    (f"id_{i}", emb, {"text": chunk.page_content})
+    for i, (chunk, emb) in enumerate(zip(chunks, embeddings_list))
+]
+index.upsert(vectors=vectors_to_upsert)
+print("✅ Transcript chunks upserted to Pinecone.")
